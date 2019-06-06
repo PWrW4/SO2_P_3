61113ea254d906a5284797d5420fdd79c8eadd13
lpawelcz <telerich97@gmail.com>
2019-06-07 01:46:02 +0200

    WC

--- Toilet.cpp
+++ Toilet.cpp
@@ -4,8 +4,27 @@
 	Toilet::Toilet(std::string name, int capacity)
         : Room(name, capacity, RoomType::E_Toilet)
     {
-
+        cubicle = new bool[CUBICLE_CNT];
+        urinal = new bool[URINAL_CNT];
     }
 
-    Toilet::Toilet(){}
-    Toilet::~Toilet(){} 
+    Toilet::Toilet()
+    {
+        cubicle = new bool[CUBICLE_CNT];
+        urinal = new bool[URINAL_CNT];
+    }
+
+    Toilet::~Toilet()
+    {
+        delete cubicle, urinal;
+    } 
+
+    void Occupy(bool *type, int index)
+    {
+        type[index] = true;
+    }
+
+    void Free(bool *type, int index)
+    {
+        type[index] = false;
+    }
--- Toilet.hpp
+++ Toilet.hpp
@@ -1,10 +1,18 @@
 #include <string>
 #include "Room.hpp"
+#define CUBICLE_CNT 2
+#define URINAL_CNT 4
 
 class Toilet : public Room
 {
     public:
+    bool *cubicle;
+    bool *urinal;
+
     Toilet(std::string name, int capacity);
     Toilet();
     ~Toilet();
-}; 
+
+    void Occupy(bool *type, int index);
+    void Free(bool *type, int index);
+}
