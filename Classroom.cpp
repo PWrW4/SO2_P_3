bef87bdd488c168e8d6d4b69815dd260835ba212
Wojciech Wójcik <wojtek.wroclaw@hotmail.com>
2019-06-13 18:24:43 +0200

    rysowania stanowisk w klasie

--- Classroom.cpp
+++ Classroom.cpp
@@ -9,7 +9,20 @@ Classroom::Classroom(std::string name, int capacity, Visualization *Display, int
     v->disp_mutex->lock();
     x = v->ClassRoomX;
     y = v->ClassRoomY;
+    y=id*9;
     v->disp_mutex->unlock();
+
+    mtx_nauczyciel.lock();
+    mtx_stanowiska.lock();
+    nauczyciel.resize(8);
+    stanowiska.resize(8);
+    for (int i = 0; i < 8; i++)
+    {
+        nauczyciel[i] == nullptr;
+        stanowiska[i] == nullptr;
+    }
+    mtx_nauczyciel.unlock();
+    mtx_stanowiska.unlock();    
 
     thread thr(&Classroom::drawClass, this);
 	std::swap(thr, t);
@@ -17,6 +30,120 @@ Classroom::Classroom(std::string name, int capacity, Visualization *Display, int
 
 void Classroom::drawClass(){
 
+    while (true)
+    {
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        for (int i = 0; i < 8; i++)
+        {
+            drawStanowisko(i);               
+        }
+    }
+}
+
+void Classroom::drawStanowisko(int nr){
+    int cx=0;
+    int cy=0;
+
+    if (nr == 0)
+    {
+        cx = x + 1;
+        cy = y + 1;
+    }
+    if (nr == 1)
+    {
+        cx = x + 1;
+        cy = y + 8;
+    }
+    if (nr == 2)
+    {
+        cx = x + 4;
+        cy = y + 1;
+    }
+    if (nr == 3)
+    {
+        cx = x + 4;
+        cy = y + 8;
+    }
+    if (nr == 4)
+    {
+        cx = x + 7;
+        cy = y + 1;
+    }
+    if (nr == 5)
+    {
+        cx = x + 7;
+        cy = y + 8;
+    }
+    if (nr == 6)
+    {
+        cx = x + 10;
+        cy = y + 1;
+    }
+    if (nr == 7)
+    {
+        cx = x + 10;
+        cy = y + 8;
+    }
+
+    if (nr % 2 == 0)
+    {
+        mtx_nauczyciel.lock();
+        if (nauczyciel[nr] == nullptr)
+        {
+            v->PutChar(cy+1,cx," ");
+        }
+        else
+        {
+            v->PutChar(cy+1,cx,"L");
+        }
+        mtx_nauczyciel.unlock();
+    }
+    else
+    {
+        mtx_nauczyciel.lock();
+        if (nauczyciel[nr] == nullptr)
+        {
+            v->PutChar(cy-1,cx," ");
+        }
+        else
+        {
+            v->PutChar(cy-1,cx,"L");
+        }
+        mtx_nauczyciel.unlock();
+    }
+
+
+
+    if (nr % 2 == 0)
+    {
+        mtx_stanowiska.lock();
+        if (stanowiska[nr] == nullptr)
+        {
+            v->PutChar(cy,cx+1," ");
+        }
+        else
+        {
+            v->PutChar(cy,cx+1,"S");
+        }
+        mtx_stanowiska.unlock();
+    }
+    else
+    {
+        mtx_stanowiska.lock();
+        if (stanowiska[nr] == nullptr)
+        {
+            v->PutChar(cy,cx+1," ");
+        }
+        else
+        {
+            v->PutChar(cy,cx+1,"S");
+        }
+        mtx_stanowiska.unlock();
+    }
+}
+
+void Classroom::studentEnter(Student * s){
+    
 }
 
 Classroom::Classroom() {}
--- Classroom.hpp
+++ Classroom.hpp
@@ -1,12 +1,14 @@
 #pragma once
 #include <string>
 #include <vector>
-#include <iostream>
 #include <thread>
 #include "Visualization.hpp"
 #include "Room.hpp"
 #include "Person.hpp"
-
+#include "Student.hpp"
+#include "Lecturer.hpp"
+#include <queue>
+#include <mutex>
 
 class Person;
 
@@ -23,7 +25,18 @@ public:
     Visualization * v;
     int x,y;
 
+
+    std::mutex mtx_stanowiska;
+    std::vector<Student *> stanowiska;
+    std::mutex mtx_nauczyciel;
+    std::vector<Lecturer *> nauczyciel;
+    std::mutex mtx_q;
+    std::queue <int> q;
+
+    void studentEnter(Student * s);
+
     void drawClass();
+    void drawStanowisko(int nr);
 
     void virtual_function();
 
